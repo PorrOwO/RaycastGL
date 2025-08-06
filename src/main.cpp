d80f87f53d31d4eb7a304261c23cc2ebfb7e2e46
PorrOwO <andrea.dalcin04@gmail.com>
2025-08-07 00:38:01 +0200

    add shader class, TODO fix shader path

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include "headers/shader.h"
 
 // CONSTANTS
 #define WINDOW_WIDTH 800
@@ -8,25 +9,17 @@
 
 #define MAP_SIZE 8
 
-// STRUCTS
-struct Map {
-  int cellSize = 64;
-  int data[MAP_SIZE][MAP_SIZE] = {
-    {1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 1, 0, 0, 1},
-    {1, 0, 0, 0, 1, 0, 0, 1},
-    {1, 1, 0, 0, 1, 1, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1}
-  };
+// DATA
+float vertices[] = {
+   0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f, //top right
+   0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, //bottom right
+  -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, //bottom left
+  //-0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 1.0f, //top left
 };
 
-struct Player {
-  float x = 0.6f;
-  float y = 0.6f;
-  //float angle = 0.0f; // in radians
+unsigned int indices[] = {
+  0, 1, 3, //first triangle
+  1, 2, 3  //second triangle  
 };
 
 // FUNCTION PROTOTYPES
@@ -35,14 +28,12 @@ void processInput(GLFWwindow*);
 
 
 int main(int arc, char* argv[]) {
-  Map map;
-  Player player;
-
+  //GLFW initialization
   if(!glfwInit()){
     std::cerr << "error: GLFW init failed" << std::endl;
     return -1;
   }
-  GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "OpenGL Window", nullptr, nullptr);
+  GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_WIDTH, "OpenGL Window", nullptr, nullptr);
   if(!window){
     std::cerr << "error: GLFW window creation failed" << std::endl;
     glfwTerminate();
@@ -50,13 +41,41 @@ int main(int arc, char* argv[]) {
   }
   
   glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, frame_buffer_size_callback);
+  
+  //initialize GLAD
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
     std::cout << "Failed to initialize GLAD" << std::endl;
     return -1;
   }  
 
-  glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
-  glfwSetFramebufferSizeCallback(window, frame_buffer_size_callback);
+  //shader class initialization
+  Shader shader = Shader("/home/porrowo/Documents/computer_graphics/raycasterGL/src/shaders/vShader.glsl", 
+                      "/home/porrowo/Documents/computer_graphics/raycasterGL/src/shaders/fShader.glsl");
+  
+  //generate vertex buffer object
+  unsigned int VBO, VAO, EBO;
+  glGenVertexArrays(1, &VAO);
+  
+  glGenBuffers(1, &VBO);
+  //glGenBuffers(1, &EBO);
+    
+  //bind VAO
+  glBindVertexArray(VAO);
+  //bind VBO to array buffer
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  //glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+  //position attribute
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+  //color attribute
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
+
 
   //render loop
   while(!glfwWindowShouldClose(window)){
@@ -66,16 +85,19 @@ int main(int arc, char* argv[]) {
     //rendering
     glClearColor(0.1f, 0.2f, 0.6f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
-
+    
+    //draw triangle
+    shader.use();
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
     //check events and swap buffers
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
-  
-
-
-
-
+ 
+  //cleanup
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
   glfwTerminate();
   return 0;
 }
@@ -84,7 +106,6 @@ int main(int arc, char* argv[]) {
 void frame_buffer_size_callback(GLFWwindow* win, int width, int height){
   glViewport(0, 0, width, height);
 }
-
 
 void processInput(GLFWwindow* win){
   if(glfwGetKey(win, GLFW_KEY_ESCAPE) == GLFW_PRESS){
